6658dbc48c7ba82401d494e65aead6df747c8821
Anton Kostiukhin <toshaklg@gmail.com>
2023-12-25 19:01:41 +0200

    Visualizations rename, extra layers

--- src/components/sidepanel/DataLayerEntry.vue
+++ src/components/sidepanel/DataLayerEntry.vue
@@ -16,15 +16,21 @@
 import { ref } from "vue"
 
 import blueprint_scene from "/src/assets/scenes/blueprint_scene.jpg"
-import vv_ground from "/src/assets/visualizations/vv_ground.jpg"
-import vh_ground from "/src/assets/visualizations/vh_ground.jpg"
-import vh_over_vv_ground from "/src/assets/visualizations/vh_over_vv_ground.jpg"
+import precipitation_blue from "/src/assets/visualizations/precipitation_blue.jpg"
+import s1_vv from "/src/assets/visualizations/s1_vv.jpg"
+import s1_vh from "/src/assets/visualizations/s1_vh.jpg"
+import s1_vh_over_vv from "/src/assets/visualizations/s1_vh_over_vv.jpg"
+import s2_tci from "/src/assets/visualizations/s2_tci.jpg"
+import s2_ndvi from "/src/assets/visualizations/s2_ndvi.jpg"
 
 const covers = {
   "blueprint_scene": blueprint_scene,
-  "vv_ground": vv_ground,
-  "vh_ground": vh_ground,
-  "vh_over_vv_ground": vh_over_vv_ground,
+  "precipitation_blue": precipitation_blue,
+  "s1_vv": s1_vv,
+  "s1_vh": s1_vh,
+  "s1_vh_over_vv": s1_vh_over_vv,
+  "s2_tci": s2_tci,
+  "s2_ndvi": s2_ndvi,
 }
 
 const isOpen = ref(false)
