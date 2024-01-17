8c66a3116bc4212a1b02926631788c29b117fe3d
Anton Kostiukhin <toshaklg@gmail.com>
2024-01-17 22:34:42 +0200

    [0.1.2] Deploy

--- src/components/sidebar/legendpanel/BkWt.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div class="w-full h-3 bg-gradient-to-r from-[#000000] from-0% to-[#ffffff] to-100%"></div>
-    <div class="flex items-center justify-between w-full text-textPrimary">
-      <span class="text-sm font-medium">Lower intensity</span>
-      <span class="text-sm font-medium">Higher intensity</span>
-    </div>
-  </div>
-</template>
--- src/components/sidebar/legendpanel/BnYlGn.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div class="w-full h-3 bg-gradient-to-r from-[#883C1E] from-0% via-[#FDFD0C] via-50% to-[#104803] to-100%"></div>
-    <div class="flex items-center justify-between w-full text-textPrimary">
-      <span class="text-sm font-medium">Lower vegetation</span>
-      <span class="text-sm font-medium">Higher vegetation</span>
-    </div>
-  </div>
-</template>
